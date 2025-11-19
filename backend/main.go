994ed4e3d580b02b3c7568d8a7b53c4e5bc4c82e
toma1128 <kirima--idea--@au.com>
2025-11-20 01:28:09 +0900

    [update]envファイルからポートを読み込むように変更

--- backend/main.go
+++ backend/main.go
@@ -2,12 +2,23 @@ package main
 
 import (
 	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 
 	"grease_trace/application/usecase"
 	"grease_trace/infrastructure/persistence"
 	"grease_trace/infrastructure/router"
 	"grease_trace/interfaces/controller"
 )
+
+func envLoad() {
+	err := godotenv.Overload(".env")
+	if err != nil {
+		log.Fatalf("ファイルが見つからないため環境変数で実行します")
+
+	}
+}
 
 func main() {
 	// 1. Infrastructure: データベース(今回はメモリ)の準備
@@ -16,13 +27,21 @@ func main() {
 	// 2. Application: ユースケースの準備 (Repoを注入)
 	interactor := usecase.NewSessionInteractor(repo)
 
+	// envファイル読み込み
+	envLoad()
+
 	// 3. Interface: コントローラーの準備 (Usecaseを注入)
 	ctrl := controller.NewSessionController(interactor)
 
 	// 4. Infrastructure: ルーターの準備 (Controllerを注入)
 	r := router.NewRouter(ctrl)
 
+	GOPORT := os.Getenv("GO_PORT")
+	if GOPORT == "" {
+		GOPORT = "8080" // デフォルトポート
+	}
+
 	// 5. サーバー起動
-	log.Println("Go APIサーバーをポート 8800 で起動します...")
-	r.Run(":8800")
+	log.Println("Go APIサーバーをポート " + GOPORT + " で起動します...")
+	r.Run(":" + GOPORT)
 }
